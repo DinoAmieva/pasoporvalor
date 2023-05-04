d48f832be78dc95bf4b23aa0b8e1c876b16f270c
Dino Amieva <125599893+DinoAmieva@users.noreply.github.com>
2023-05-04 12:54:08 -0600

    Add files via upload

---
+++ Pasoporvalor.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasoporvalor;
+impublic class Main {
+
+     public static void method(int x, String y, List<String> w, List<String> z) {
+         x = 999;
+         y = "Hola Mundo!";
+         w = List.of("Hola", "mundo", "!");
+         z.addAll(List.of("Hola", "mundo", "!"));
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        String b = "Hello World!";
+        List<String> c = Arrays.asList("Hello", "World", "!");
+        List<String> d = new ArrayList(List.of("Hello", "World", "!"));
+        
+        Main.method(a, b, c, d);
+        
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
+    }
+ }
+}ort java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Lis
+/**
+ *
+ * @author Sac2-20
+ */
+public class Pasoporvalor {
+
+public class Main {
+
+     public static void method(int x, String y, List<String> w, List<String> z) {
+         x = 999;
+         y = "Hola Mundo!";
+         w = List.of("Hola", "mundo", "!");
+         z.addAll(List.of("Hola", "mundo", "!"));
+    }
+
+    public static void main(String[] args) {
+        int a = 1;
+        String b = "Hello World!";
+        List<String> c = Arrays.asList("Hello", "World", "!");
+        List<String> d = new ArrayList(List.of("Hello", "World", "!"));
+        
+        Main.method(a, b, c, d);
+        
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
+    }
+ }
+}
